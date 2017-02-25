99b6ac1dec2b3afcbb684ef1a72fceff2b8103b5
ahmed.jalal93 <ajalal@inthinc.com>
2017-02-25 14:22:29 -0700

    First commit

---
+++ app/src/main/java/org/wetube/rtmpplayer/MainActivity.java
@@ -0,0 +1,281 @@
+package org.wetube.rtmpplayer;
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.audio.AudioCapabilitiesReceiver;
+import com.google.android.exoplayer.util.Util;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
+/**
+ * An activity that plays media using {@link DemoPlayer}.
+ */
+public class MainActivity extends Activity implements SurfaceHolder.Callback, View.OnClickListener, DemoPlayer.Listener,
+        AudioCapabilitiesReceiver.Listener {
+
+    private static final String TAG = "PlayerActivity";
+
+    private static final CookieManager defaultCookieManager;
+
+    static {
+        defaultCookieManager = new CookieManager();
+        defaultCookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+    }
+
+    private SurfaceView surfaceView;
+
+    private DemoPlayer player;
+    private boolean playerNeedsPrepare;
+
+    private long playerPosition;
+    private boolean enableBackgroundAudio = true;
+
+    private Uri contentUri;
+
+    private AudioCapabilitiesReceiver audioCapabilitiesReceiver;
+
+    // Activity lifecycle
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        View root = findViewById(R.id.root);
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    //TODO
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    view.performClick();
+                }
+                return true;
+            }
+        });
+
+        CookieHandler currentHandler = CookieHandler.getDefault();
+        if (currentHandler != defaultCookieManager) {
+            CookieHandler.setDefault(defaultCookieManager);
+        }
+
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        surfaceView.getHolder().addCallback(this);
+
+        audioCapabilitiesReceiver = new AudioCapabilitiesReceiver(this, this);
+        audioCapabilitiesReceiver.register();
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        releasePlayer();
+        playerPosition = 0;
+        setIntent(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            onShown();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Util.SDK_INT <= 23 || player == null) {
+            onShown();
+        }
+    }
+
+    private void onShown() {
+
+        contentUri = Uri.parse("rtmp://www.teledunet.net:1935/live?idu=ahmedjalal2016_65414728080/mbc_2 live=1");
+        if (player == null) {
+            preparePlayer(true);
+        } else {
+            player.setBackgrounded(false);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            onHidden();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            onHidden();
+        }
+    }
+
+    private void onHidden() {
+        if (!enableBackgroundAudio) {
+            releasePlayer();
+        } else {
+            player.setBackgrounded(true);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        audioCapabilitiesReceiver.unregister();
+        releasePlayer();
+    }
+
+    // OnClickListener methods
+
+    @Override
+    public void onClick(View view) {
+        preparePlayer(true);
+    }
+
+    // AudioCapabilitiesReceiver.Listener methods
+
+    @Override
+    public void onAudioCapabilitiesChanged(AudioCapabilities audioCapabilities) {
+        if (player == null) {
+            return;
+        }
+        boolean backgrounded = player.getBackgrounded();
+        boolean playWhenReady = player.getPlayWhenReady();
+        releasePlayer();
+        preparePlayer(playWhenReady);
+        player.setBackgrounded(backgrounded);
+    }
+
+    // Permission request listener method
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            preparePlayer(true);
+        } else {
+            Toast.makeText(getApplicationContext(), "denied",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private void preparePlayer(boolean playWhenReady) {
+        String userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
+        if (player == null) {
+            player = new DemoPlayer(new ExtractorRendererBuilder(this, userAgent, contentUri));
+            player.addListener(this);
+            player.seekTo(playerPosition);
+            playerNeedsPrepare = true;
+        }
+        if (playerNeedsPrepare) {
+            player.prepare();
+            playerNeedsPrepare = false;
+        }
+        player.setSurface(surfaceView.getHolder().getSurface());
+        player.setPlayWhenReady(playWhenReady);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    // DemoPlayer.Listener implementation
+
+    @Override
+    public void onStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == ExoPlayer.STATE_ENDED) {
+            //TODO
+        }
+        String text = "playWhenReady=" + playWhenReady + ", playbackState=";
+        switch (playbackState) {
+            case ExoPlayer.STATE_BUFFERING:
+                text += "buffering";
+                break;
+            case ExoPlayer.STATE_ENDED:
+                text += "ended";
+                break;
+            case ExoPlayer.STATE_IDLE:
+                text += "idle";
+                break;
+            case ExoPlayer.STATE_PREPARING:
+                text += "preparing";
+                break;
+            case ExoPlayer.STATE_READY:
+                text += "ready";
+                break;
+            default:
+                text += "unknown";
+                break;
+        }
+    }
+
+    @Override
+    public void onError(Exception e) {
+        playerNeedsPrepare = true;
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthAspectRatio) {
+//        videoFrame.setAspectRatio(height == 0 ? 1 : (width * pixelWidthAspectRatio) / height);
+    }
+
+
+    // SurfaceHolder.Callback implementation
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (player != null) {
+            player.setSurface(holder.getSurface());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Do nothing.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (player != null) {
+            player.blockingClearSurface();
+        }
+    }
+
+
+}
